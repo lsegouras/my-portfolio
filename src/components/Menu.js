fe436b9b0da12bf27a77346b6432a7d1dc2ffa36
Laura Segouras <lausegouras@yahoo.com.br>
2023-06-27 14:28:14 -0300

    toggler color correction

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -9,7 +9,7 @@ const Menu = () => {
 <Navbar expand="lg" className="navbar">
       <Container>
         <Navbar.Brand className="navbar-brand" href="#home">Portfolio de Laura Segouras</Navbar.Brand>
-        <Navbar.Toggle className="bg-light" aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle style={{ backgroundColor: "#F2C6A0"}} aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto" id="nav">
             <Nav.Link className="nav-link" href="#Sobre Mim">Sobre Mim</Nav.Link>
@@ -23,60 +23,5 @@ const Menu = () => {
     </Navbar>
   );
 }
-
-
-
-
-
-//     <nav className='navbar navbar-expand-lg navbar-light'>
-//       <div className='container'>
-//         <a className='navbar-brand' href='#'>
-//           Laura Segoura's Portfolio
-//         </a>
-//         <button
-//           className='navbar-toggler'
-//           type='button'
-//           data-toggle='collapse'
-//           data-target='#navbarSupportedContent'
-//           aria-controls='navbarSupportedContent'
-//           aria-expanded='false'
-//           aria-label='Toggle navigation'
-//         >
-//           <span className='navbar-toggler-icon'></span>
-//         </button>
-
-//         <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-//           <ul className='navbar-nav ms-auto'>
-//             <li className='nav-item active'>
-//               <a className='nav-link' href='#'>
-//                 sobre Mim
-//               </a>
-//             </li>
-//             <li className='nav-item'>
-//               <a className='nav-link' href='#'>
-//                 Objetivos
-//               </a>
-//             </li>
-//             <li className='nav-item'>
-//               <a className='nav-link' href='#'>
-//                 Experiência
-//               </a>
-//             </li>
-//             <li className='nav-item'>
-//               <a className='nav-link' href='#'>
-//                 Portfolio
-//               </a>
-//             </li>
-//             <li className='nav-item'>
-//               <a className='nav-link' href='#'>
-//                 Contatos
-//               </a>
-//             </li>
-//           </ul>
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// };
 
 export default Menu;
