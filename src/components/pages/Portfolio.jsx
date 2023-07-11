cfa622c3009e8e9551994ac8fe2855494132354e
Laura Segouras <lausegouras@yahoo.com.br>
2023-07-11 16:33:12 -0300

    Portfolio section improvements

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -14,167 +14,169 @@ const Portfolio = () => {
             <img src={project1} alt='project 1' />
             <h5>APP de Portfólio de Trabalho</h5>
           </div>
-          <p>
-            Descrição: Desenvolvimento desse próprio app do meu portfólio de
-            trabalho.
-          </p>
-          <p>
-            Tecnologias utilizadas: HTML, CSS, Javascript, React, Bootstrap e
-            bibliotecas do React, como: react-icons e react-ts-particles,
-            react-typed e react-vertical-timeline-component.
-          </p>
-          <div className="portfolio-links">
-          <a
-            href='https://github.com/lsegouras'
-            className='portfolio-link'
-            target='_blank'
-            rel='noreferrer'
-          >
-            Github
-          </a>
-          <a
-            href='http://drible.com/Aliens_pixels'
-            className='portfolio-link'
-            target='_blank'
-            rel='noreferrer'
-          >
-            Demo
-          </a>
+          <div className='project-info'>
+            <p>
+              <br />
+              Descrição: Desenvolvimento desse próprio app do meu portfólio de
+              trabalho.
+            </p>
+            <p>
+              Tecnologias utilizadas: HTML, CSS, Javascript, React, Bootstrap e
+              bibliotecas do React, como: react-icons e react-ts-particles,
+              react-typed e react-vertical-timeline-component.
+            </p>
+          </div>
+          <div className='portfolio-links'>
+            <a
+              href='https://github.com/lsegouras'
+              className='portfolio-link'
+              target='_blank'
+              rel='noreferrer'
+            >
+              Github
+            </a>
+            <a
+              href='http://drible.com/Aliens_pixels'
+              className='portfolio-link'
+              target='_blank'
+              rel='noreferrer'
+            >
+              Demo
+            </a>
+          </div>
+        </article>
+        <article className='portfolio-item'>
+          <div className='portfolio-item-img-c'>
+            <img src={project1} alt='project 1' />
+            <h5>APP de Registro de Usuários com API de Usuários</h5>
+          </div>
+          <div className='project-info-c'>
+            <p>
+              Descrição: desenvolvimento de um app de registro de usuários
+              (login), com interface para preenchimento de formulário pelo
+              usuário e botão para deletar o usuário, com deploy no Netlify.
+              Desenvolvimento de API simples de registro de usuários.
+            </p>
+            <p>
+              Tecnologias utilizadas: front-end – HTML, CSS, Javascript e React
+              / back-end: Node.js e Express.
+            </p>
+          </div>
+          <div className='portfolio-links-c'>
+            <a
+              href='https://github.com/lsegouras'
+              className='portfolio-link'
+              target='_blank'
+              rel='noreferrer'
+            >
+              Github
+            </a>
+            <a
+              href='http://drible.com/Aliens_pixels'
+              className='portfolio-link'
+              target='_blank'
+              rel='noreferrer'
+            >
+              Demo
+            </a>
           </div>
         </article>
         <article className='portfolio-item'>
           <div className='portfolio-item-img'>
             <img src={project1} alt='project 1' />
-            <h5>APP de Portfólio de Trabalho</h5>
+            <h5>Site Institucional</h5>
           </div>
-          <p>
-            Descrição: Desenvolvimento desse próprio app do meu portfólio de
-            trabalho.
-          </p>
-          <p>
-            Tecnologias utilizadas: HTML, CSS, Javascript, React, Bootstrap e
-            bibliotecas do React, como: react-icons e react-ts-particles,
-            react-typed e react-vertical-timeline-component.
-          </p>
-          <div className="portfolio-links">
-          <a
-            href='https://github.com/lsegouras'
-            className='portfolio-link'
-            target='_blank'
-            rel='noreferrer'
-          >
-            Github
-          </a>
-          <a
-            href='http://drible.com/Aliens_pixels'
-            className='portfolio-link'
-            target='_blank'
-            rel='noreferrer'
-          >
-            Demo
-          </a>
+          <div className='project-info'>
+            <p>
+            <br />
+              Descrição: Desenvolvimento de site institucional responsivo de
+              acordo com especificações de layout e cores solicitados pelo
+              cliente.
+            </p>
+            <p>Tecnologias utilizadas: HTML, CSS e Javascript.</p>
           </div>
-        </article>
-        <article className='portfolio-item'>
-          <div className='portfolio-item-img'>
-            <img src={project1} alt='project 1' />
-            <h5>APP de Portfólio de Trabalho</h5>
-          </div>
-          <p>
-            Descrição: Desenvolvimento desse próprio app do meu portfólio de
-            trabalho.
-          </p>
-          <p>
-            Tecnologias utilizadas: HTML, CSS, Javascript, React, Bootstrap e
-            bibliotecas do React, como: react-icons e react-ts-particles,
-            react-typed e react-vertical-timeline-component.
-          </p>
-          <div className="portfolio-links">
-          <a
-            href='https://github.com/lsegouras'
-            className='portfolio-link'
-            target='_blank'
-            rel='noreferrer'
-          >
-            Github
-          </a>
-          <a
-            href='http://drible.com/Aliens_pixels'
-            className='portfolio-link'
-            target='_blank'
-            rel='noreferrer'
-          >
-            Demo
-          </a>
+          <div className='portfolio-links'>
+            <a
+              href='https://github.com/lsegouras'
+              className='portfolio-link'
+              target='_blank'
+              rel='noreferrer'
+            >
+              Github
+            </a>
+            <a
+              href='http://drible.com/Aliens_pixels'
+              className='portfolio-link'
+              target='_blank'
+              rel='noreferrer'
+            >
+              Demo
+            </a>
           </div>
         </article>
         <article className='portfolio-item'>
           <div className='portfolio-item-img'>
             <img src={project1} alt='project 1' />
-            <h5>APP de Portfólio de Trabalho</h5>
+            <h5>APP de Conselhos</h5>
           </div>
-          <p>
-            Descrição: Desenvolvimento desse próprio app do meu portfólio de
-            trabalho.
-          </p>
-          <p>
-            Tecnologias utilizadas: HTML, CSS, Javascript, React, Bootstrap e
-            bibliotecas do React, como: react-icons e react-ts-particles,
-            react-typed e react-vertical-timeline-component.
-          </p>
-          <div className="portfolio-links">
-          <a
-            href='https://github.com/lsegouras'
-            className='portfolio-link'
-            target='_blank'
-            rel='noreferrer'
-          >
-            Github
-          </a>
-          <a
-            href='http://drible.com/Aliens_pixels'
-            className='portfolio-link'
-            target='_blank'
-            rel='noreferrer'
-          >
-            Demo
-          </a>
+          <div className='project-info'>
+            <p>
+            <br />
+              Descrição: desenvolvimento de app de conselhos aleatórios
+              consumindo API externa (https://api.adviceslip.com/advice)
+            </p>
+            <p>Tecnologias Utilizadas: HTML, CSS e Javascript.</p>
+          </div>
+          <div className='portfolio-links'>
+            <a
+              href='https://github.com/lsegouras'
+              className='portfolio-link'
+              target='_blank'
+              rel='noreferrer'
+            >
+              Github
+            </a>
+            <a
+              href='http://drible.com/Aliens_pixels'
+              className='portfolio-link'
+              target='_blank'
+              rel='noreferrer'
+            >
+              Demo
+            </a>
           </div>
         </article>
         <article className='portfolio-item'>
           <div className='portfolio-item-img'>
             <img src={project1} alt='project 1' />
-            <h5>APP de Portfólio de Trabalho</h5>
+            <h5>APP de validação de formulário</h5>
           </div>
-          <p>
-            Descrição: Desenvolvimento desse próprio app do meu portfólio de
-            trabalho.
-          </p>
-          <p>
-            Tecnologias utilizadas: HTML, CSS, Javascript, React, Bootstrap e
-            bibliotecas do React, como: react-icons e react-ts-particles,
-            react-typed e react-vertical-timeline-component.
-          </p>
-          <div className="portfolio-links">
-          <a
-            href='https://github.com/lsegouras'
-            className='portfolio-link'
-            target='_blank'
-            rel='noreferrer'
-          >
-            Github
-          </a>
-          <a
-            href='http://drible.com/Aliens_pixels'
-            className='portfolio-link'
-            target='_blank'
-            rel='noreferrer'
-          >
-            Demo
-          </a>
+          <div className='project-info'>
+          <br />
+            <p>
+              Descrição: desenvolvimento de um app de validação de formulário.
+            </p>
+            <p>Tecnologias Utilizadas: HTML, CSS e Javascript.</p>
+          </div>
+          <div className='portfolio-links'>
+            <a
+              href='https://github.com/lsegouras'
+              className='portfolio-link'
+              target='_blank'
+              rel='noreferrer'
+            >
+              Github
+            </a>
+            <a
+              href='http://drible.com/Aliens_pixels'
+              className='portfolio-link'
+              target='_blank'
+              rel='noreferrer'
+            >
+              Demo
+            </a>
           </div>
         </article>
-        
       </div>
     </section>
   );
